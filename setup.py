38173f76ac2573dd5e1108e0159cbee9cbe8efad
Alex Dolphin <trteam1988@gmail.com>
2019-09-29 12:59:01 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fantasy-database-2019-thecasper2",
+    version="0.0.1",
+    author="Alex Dolphin",
+    author_email="alex.dolphin@dolphindata.co.uk",
+    description="Writes Fantasy Football API data to a dtabase",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thecasper2/fantasy-database-2019",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
