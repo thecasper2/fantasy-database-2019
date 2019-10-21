a59b658a9fae3b489285a13b0748daf80550b4df
Alex Dolphin <trteam1988@gmail.com>
2019-10-21 11:01:30 +0200

    Implement player history table class

---
+++ src/fantasy/database/player_history_table.py
@@ -0,0 +1,27 @@
+from tqdm import tqdm
+
+from .base_table import BaseTable
+from .tables import player_history
+from src.fantasy.api.bootstrap_static_api import BootstrapStaticAPI
+from src.fantasy.api.player_history_api import PlayerHistoryAPI
+
+
+class PlayerHistoryTable(BaseTable):
+	"""
+	A class for performing methods on the team table
+	"""
+
+	def __init__(self):
+		"""
+		Firstly we get all player ID values from the Bootstrap static API. We then fire a request to the history API
+		for each player ID and bind the result to make a very large data frame of player history.
+		"""
+		bootstrap = BootstrapStaticAPI()
+		player_ids = bootstrap.player_ids()
+		player_history_data = []
+		for player_id in tqdm(player_ids):
+			player_history_data.extend(PlayerHistoryAPI(player_id=player_id).history())
+		super(PlayerHistoryTable, self).__init__(
+			table=player_history,
+			content=player_history_data
+		)
