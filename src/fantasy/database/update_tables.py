f651ed35a4528c01a0c0e7001efdebe936d72632
Alex Dolphin <trteam1988@gmail.com>
2019-11-04 12:17:29 +0100

    Store table class as variable before upating

--- src/fantasy/database/update_tables.py
+++ src/fantasy/database/update_tables.py
@@ -10,9 +10,12 @@ if __name__ == "__main__":
 	If this script is invoked directly, it will create an instance of each table class, then call the update_table
 	method on each to write to MySQL
 	"""
-	stdout.write("Updating team table")
-	TeamTable().update_table()
-	stdout.write("Updating player table")
-	PlayerTable().update_table()
-	stdout.write("Updating player history table")
-	PlayerHistoryTable().update_table()
+	stdout.write("Updating team table\n")
+	t = TeamTable()
+	t.update_table()
+	stdout.write("Updating player table\n")
+	p = PlayerTable()
+	p.update_table()
+	stdout.write("Updating player history table\n")
+	ph = PlayerHistoryTable()
+	ph.update_table()
